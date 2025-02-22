3f34cf66c542a52b82a588b19c989235da04ccc0
Gautamverma2003 <gautam146official@gmail.com>
2025-02-22 11:51:55 +0530

    [Gautam] add.Added hello with PAth variable

--- springintro/src/main/java/com/Capgemini/springintro/controller/HelloFromBridgelabz.java
+++ springintro/src/main/java/com/Capgemini/springintro/controller/HelloFromBridgelabz.java
@@ -1,6 +1,9 @@
 package com.Capgemini.springintro.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +17,10 @@ public class HelloFromBridgelabz {
     @GetMapping("/hello/query")
     public String sayHello(@RequestParam(value = "name",defaultValue = "user") String name){
         return "Hello" + name + "From Bridgelabz";
+    }
+
+    @GetMapping("hello1/{name}")
+    public ResponseEntity<String> hello1(@PathVariable("name")String name){
+        return new ResponseEntity<>("Hello  " + name + " from BridgeLabz", HttpStatus.OK );
     }
 }

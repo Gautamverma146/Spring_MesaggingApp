b065b6fcccbcc061f7a73e0f3b0a4617a956a53c
Gautamverma2003 <gautam146official@gmail.com>
2025-02-22 11:32:49 +0530

    [Gautam] add.Added Say Hello Mark from bridgelabz

--- springintro/src/main/java/com/Capgemini/springintro/controller/HelloFromBridgelabz.java
+++ springintro/src/main/java/com/Capgemini/springintro/controller/HelloFromBridgelabz.java
@@ -1,6 +1,7 @@
 package com.Capgemini.springintro.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +9,10 @@ public class HelloFromBridgelabz {
     @GetMapping("/hello")
     public String hello(){
         return "Hello From Bridgelabz";
+    }
+
+    @GetMapping("/hello/query")
+    public String sayHello(@RequestParam(value = "name",defaultValue = "user") String name){
+        return "Hello" + name + "From Bridgelabz";
     }
 }
